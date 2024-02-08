59e9f19fc554e729611e1efcbd2667e438057b50
Keith Rich <keithrich18@gmail.com>
2024-02-08 15:48:03 -0800

    02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR"
+    }
+    let rep = ""
+    for(i = 0; i < num; i++){
+        rep = rep.concat(str)
+    }
+    return rep;
 };
 
 // Do not edit below this line
