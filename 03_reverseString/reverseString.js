5ea546f02b21a4f5e67623d3d57f3d03dc862ee0
Keith Rich <keithrich18@gmail.com>
2024-02-08 16:12:37 -0800

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    rev = "";
+    for (i = str.length - 1; i >= 0; i--){
+        rev += str[i];
+    }
+    return rev
 
 };
 
