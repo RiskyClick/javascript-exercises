cd786272a256290e647170bd2bf18bb0d763c6a6
Keith Rich <keithrich18@gmail.com>
2024-02-09 14:59:48 -0800

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)){
+        return "ERROR"
+    }
+    if(n1 < 0 || n2 < 0){
+        return "ERROR"
+    }
+    if(n1 > n2){
+        temp = n1;
+        n1 = n2;
+        n2 = temp
+    }
+    count = n1;
+    for(i = n1 + 1; i < n2 + 1; i++){
+        count += i;
+    }
+    return count;
 };
 
 // Do not edit below this line
