6e18eafb422083b4042b11d4c7887808c83169e8
Keith Rich <keithrich18@gmail.com>
2024-02-08 16:57:14 -0800

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...args) {
+    for(i = 0; i < args.length; i++){
+        for(j = 0; j < list.length; j++){
+            if (args[i] === list[j]){
+                list.splice(j, 1)
+                j--;
+            }
+        }
+    }
+    return list
 };
 
 // Do not edit below this line
